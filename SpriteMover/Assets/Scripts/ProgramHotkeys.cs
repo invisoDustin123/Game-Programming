ff37d1501590e3f5b915c8c3443d9b62853a1770
Dustin Bingham <invisoDustin123@gmail.com>
2018-01-28 21:48:22 -0700

    Full sprite mover
    updated with new hotkeys and changed from WASD to arrow keys

---
+++ SpriteMover/Assets/Scripts/ProgramHotkeys.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramHotkeys : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //If Space is pushed, the ship sprite will appear in the middle of the screen
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+
+        //If P is pushed, the ship will toggle weather it's enabled
+        if (Input.GetKey(KeyCode.P))
+        {
+            GetComponent<ShipMovement>().enabled = !GetComponent<ShipMovement>().enabled;
+
+        }
+
+        //If Esc is pushed, the game will exit
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        //If Q is pushed, the ship will be set to inactive
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- SpriteMover/Assets/Scripts/ShipMovement.cs
+++ SpriteMover/Assets/Scripts/ShipMovement.cs
@@ -8,23 +8,23 @@ public class ShipMovement : MonoBehaviour
 
     void Update()
     {
-        //If A is pushed, move the sprite to the left
-        if (Input.GetKey(KeyCode.A))
+        //If Left is pushed, move the sprite to the left
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
-        //If D is pushed, move the sprite to the right
-        if (Input.GetKey(KeyCode.D))
+        //If Right is pushed, move the sprite to the right
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
-        //If W is pushed, move the sprite up
-        if (Input.GetKey(KeyCode.W))
+        //If Up is pushed, move the sprite up
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
         }
-        ////If S is pushed, move the sprite down
-        if (Input.GetKey(KeyCode.S))
+        ////If Down is pushed, move the sprite down
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
         }
